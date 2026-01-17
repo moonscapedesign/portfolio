3176fefcaae8e048f27db6b407451ff07b1825d6
binbandit <crazywolf132@gmail.com>
2026-01-17 12:58:01 +1100

    chore(project): remove vite react template setup

--- src/pages/Home.jsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-    return (
-        <div className="framer-Mgwl7">
-            <h1>Home Page</h1>
-            {/* Content will be ported here */}
-        </div>
-    )
-}
